d40e04da511c82e23967dfe8339d8f57a1dd9416
OrvalaAM <orvalavala@gmail.com>
2022-03-26 16:38:51 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul5_1302204105/Program.cs
+++ modul5_1302204105/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204105
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            SayaTubeUser user = new SayaTubeUser("Orvala");
+            SayaTubeVideo video = new SayaTubeVideo("Review Film Interstellar oleh " + user.Username);
+            video.IncreasePlayCount(20);
+            video.PrintVideoDetails();
+            Console.WriteLine();
+            
+
+            SayaTubeVideo video2 = new SayaTubeVideo("Review Film G30S/PKI oleh " + user.Username);
+            SayaTubeVideo video3 = new SayaTubeVideo("Review Film Fast and Furious oleh " + user.Username);
+            SayaTubeVideo video4 = new SayaTubeVideo("Review Film Battle of Surabaya oleh " + user.Username);
+            SayaTubeVideo video5 = new SayaTubeVideo("Review Film John Wick oleh " + user.Username);
+            SayaTubeVideo video6 = new SayaTubeVideo("Review Film Transformer oleh " + user.Username);
+            SayaTubeVideo video7 = new SayaTubeVideo("Review Film Terminator oleh " + user.Username);
+            SayaTubeVideo video8 = new SayaTubeVideo("Review Film Bang Dream Live2nd oleh " + user.Username);
+            SayaTubeVideo video9 = new SayaTubeVideo("Review Film Sherlock Holmes oleh " + user.Username);
+            SayaTubeVideo video10 = new SayaTubeVideo("Review Film Avatar oleh " + user.Username);
+            user.AddVideo(video);
+            user.AddVideo(video2);
+            user.AddVideo(video3);
+            user.AddVideo(video4);
+            user.AddVideo(video5);
+            user.AddVideo(video6);
+            user.AddVideo(video7);
+            user.AddVideo(video8);
+            user.AddVideo(video9);
+            user.AddVideo(video10);
+            user.PrintAllVideoPlayCount();
+
+        }
+    }
+}
---
+++ modul5_1302204105/SayaTubeUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204105
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public SayaTubeUser() { }
+        public SayaTubeUser(string user)
+        {
+            Username = user;
+            Random rnd = new Random();
+            id = rnd.Next(10000, 100000);
+            uploadedVideos = new List<SayaTubeVideo>();
+        }
+        public int GetTotalVideoPlayCount()
+        {
+            int total = 0;
+            for(int i = 0; i < uploadedVideos.Count; i++)
+            {
+                total = total + uploadedVideos[i].getPlayCount();
+            }
+            return total;
+        }
+        public void AddVideo(SayaTubeVideo video)
+        {
+            uploadedVideos.Add(video);
+        }
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine("User: " + Username);
+            for(int i = 0; i < uploadedVideos.Count; i++)
+            {
+                Console.WriteLine("Video " + (i+1) + " judul: " + uploadedVideos[i].getTitle());
+            }
+        }
+    }
+}
